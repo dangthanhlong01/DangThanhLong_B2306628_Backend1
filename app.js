3252f939ae32ab7dd62d08d79e7b9afc5c450e2e
ThanhLong <longb2306628@student.ctu.edu.vn>
2026-07-06 21:00:01 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-
+const ApiError = require('./app/api-error');
 const contactRoutes = require('./app/routes/contact.route');
 app.use(cors());
 app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({ message: "Welcome to contact book application." });
+});
 app.use('/api/contacts', contactRoutes);
 
-app.get('/', (req, res) =>{
-    res.json({message: "Welcome to contact book application."});    
+// handle 404 response
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
 });
+
+// handle error response
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    })
+})
 
 module.exports = app;
